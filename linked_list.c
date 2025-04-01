24baea0dc633f4a1f2846d664f1c9e05e5073dd6
Lourd AbuHadid <102420995+LourdAbuHadid1@users.noreply.github.com>
2025-04-01 14:05:46 +0000

    added functions of insertion and deletion

--- linked_list.c
+++ linked_list.c
@@ -1,18 +1,84 @@
 #include "linked_list.h"
-
 #include <stdlib.h>
 
 struct list_node *new_node(size_t value) { return NULL; }
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *new_node = malloc(sizeof(struct list_node));
+  if (new_node == NULL){
+    return;
+  }
+  new_node->value = value;
+  new_node->next = list->head;
+  list->head = new_node;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *new_node = malloc(sizeof(struct list_node));
+  if (new_node == NULL){
+    return;
+  }
+  new_node->value = value;
+  new_node->next = NULL;
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+  if (list->head == NULL) {
+    list->head = new_node;
+  }
+  else {
+    struct list_node *current = list->head;
+    while (current->next != NULL) {
+      current = current->next;
+    }
+    current->next = new_node;
+  }
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) {
+  if (list->head == NULL) {
+    return 0;
+  }
+  struct list_node *to_remove = list->head;
+  size_t value = to_remove->value;
 
-void free_list(struct linked_list list) {}
+  list->head = to_remove->next;
+  free(to_remove);
+  return value;
+}
+
+size_t remove_from_tail(struct linked_list *list) {
+    if (list->head == NULL) {
+        return 0;
+    }
+    
+    struct list_node *to_remove = list->head;
+    size_t value;
+    
+    if (to_remove->next == NULL) {
+        value = to_remove->value;
+        list->head = NULL;
+        free(to_remove);
+        return value;
+    }
+    
+    while (to_remove->next->next != NULL) {
+        to_remove = to_remove->next;
+    }
+    
+    value = to_remove->next->value;
+    free(to_remove->next);
+    to_remove->next = NULL;
+    
+    return value;
+}
+
+void free_list(struct linked_list list) {
+ struct list_node *current = list.head;
+    while (current != NULL) {
+        struct list_node *to_free = current;
+        current = current->next;
+        free(to_free);
+    }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
